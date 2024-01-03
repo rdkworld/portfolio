8931dfee3d5fd27159d80124914a7664e0b3fcb8
Raghavendra Kulkarni <raghavendra.kulkarni@farmersinsurance.com>
2024-01-03 15:44:24 -0800

    Merged environment files

--- genai/gemini/invoice-extractor/quickstart.py
+++ genai/gemini/invoice-extractor/quickstart.py
@@ -10,6 +10,12 @@ load_dotenv()
 #Securely store API key
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
+#Alternative HTTP REST call
+#curl \
+#  -H 'Content-Type: application/json' \
+#  -d '{"contents":[{"parts":[{"text":"Write a story about a magic backpack"}]}]}' \
+#  -X POST https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=GOOGLE_API_KEY
+
 genai.configure(api_key=GOOGLE_API_KEY)
 
 for m in genai.list_models():
