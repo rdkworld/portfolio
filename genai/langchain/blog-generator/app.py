23dfb7ecd3f4bc9aa7b9c419ef3de8b3577b2153
Raghavendra Kulkarni <raghavendra.kulkarni@farmersinsurance.com>
2024-01-05 11:19:15 -0800

    Interim update

--- genai/langchain/blog-generator/app.py
+++ genai/langchain/blog-generator/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.llms import HuggingFaceHub
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 #Load environment variables
 from dotenv import load_dotenv
@@ -18,26 +17,10 @@ def getLLamaresponse(input_text,no_words,blog_style):
     #                   model_type='llama',
     #                   config={'max_new_tokens':256,
     #                           'temperature':0.01})
-    
-    #HUGGING FACE MODEL
-    # Load model and tokenizer from Hugging Face
-    model_name = "meta-llama/Llama-2-7b-chat"  # Replace with the exact model name on Hugging Face
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-    # Create a function to handle text generation (assuming CTransformers has a `generate_text` method)
-    def generate_text(prompt, max_length=256, temperature=0.01):
-        input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        output = model.generate(
-            input_ids,
-            max_length=max_length,
-            temperature=temperature,
-    )   
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    return generated_text
-
     ## Prompt Template
 
+    llm = HuggingFaceHub(repo_id="meta-llama/Llama-2-7b-chat",model_kwargs={"temperature": 0.5, "max_length": 64})
+    
     template="""
         Write a blog for {blog_style} job profile for a topic {input_text}
         within {no_words} words.
